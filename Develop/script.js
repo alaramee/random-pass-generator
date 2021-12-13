b8fed2aecc72055037fa2f9fdc535d62e098be97
alaramee <93341641+alaramee@users.noreply.github.com>
2021-12-12 23:18:52 -0500

    added if statements

--- Develop/script.js
+++ Develop/script.js
@@ -1,35 +1,63 @@
 // Assignment code here
 
-// Variables for password criteria here
+//Undefined variable
+var passLength;
+
+// Variables for password criteria here and criteria mix
 var lowerCase = ["a", "b", "c", "d", "e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
 var numbers = ["1","2","3","4","5","6","7","8","9","0",]; 
 var symbols = ["!","#","$","%","&","(",")","*","+","-",".","/",":",";","<","=",">","?","@","^","`","|","~"]
-var charMix []
+var charMix = []
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+generateBtn.addEventListener("click", writePassword);
+
 
 // Write password to the #password input
 function writePassword() {
   var passwordGen = [];
   var passwordText = document.querySelector("#password");
   var passResult = "";
+  var initPrompt = window.prompt("Please enter the number of characters would you would like your password to contain. ");
 
-  if(selectSpecial = 'yes') {
-    var randomSpecial = symbols[Math.floor(Math.random() * symbols.length)]
-  password.push(randomSpecial);
+  passwordText.value = newPassword;
+
+  //If statements to help navigate through chosen character types
+  if (passLength < 8 | passLength > 128 | isNaN(passLength)) {
+    window.alert("This answer is not valid. Must between 8 and 128 characters.");
+    return initPrompt()
   }
-  console.log(randomSpecial);
-  console.log(password)
+
+  if (confirm("Would you like to include 'Lowercase' letters in your password?")) {
+    charMix = charMix.concat(lowerCase);
+  }
+    
+  if (confirm("Would you like to include 'Uppercase' letters in your password?")) {
+    charMix = charMix.concat(upperCase);
+  }
+
+  if (confirm("Would you like to includet 'Special Characters' in your password?")) {
+    charMix = charMix.concat(symbols);
+  }
+
+  if (confirm("Would you like to include 'Numbers' in your password?")) {
+    charMix = charMix.concat(numbers);
+  }
+
+  if (charMix.length === 0) {
+    alert("Please select at least one character type");
+    return; // getPrompts()
+  }
+
+  else {
+    for(var i=0;  i<passCriteria; i++){
+    var random = Math.floor(Math.random()*PasswordGenerator.length);
+    passResult += passwordGen[random];
+  }
+  
+  return {charLength, charMix}
+   
+  }
 }
-
-function generatePassword() {
-
-  var passLength = length();
-
-  for (var i = 0; i < passLength; i++)
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
