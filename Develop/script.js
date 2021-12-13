d3d3e5a7368bbafb9f32ee4bf3afa84d0963a8b8
alaramee <93341641+alaramee@users.noreply.github.com>
2021-12-12 22:36:27 -0500

    added functions

--- Develop/script.js
+++ Develop/script.js
@@ -5,27 +5,23 @@ var lowerCase = ["a", "b", "c", "d", "e","f","g","h","i","j","k","l","m","n","o"
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
 var numbers = ["1","2","3","4","5","6","7","8","9","0",]; 
 var symbols = ["!","#","$","%","&","(",")","*","+","-",".","/",":",";","<","=",">","?","@","^","`","|","~"]
+var charMix []
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var passwordGen = [];
   var passwordText = document.querySelector("#password");
+  var passResult = "";
 
-  passwordText.value = password;
-
+  if(selectSpecial = 'yes') {
+    var randomSpecial = symbols[Math.floor(Math.random() * symbols.length)]
+  password.push(randomSpecial);
+  }
+  console.log(randomSpecial);
+  console.log(password)
 }
-
-if(selectSpecial = 'yes') {
-  var randomSpecial = symbols[Math.floor(Math.random() * symbols.length)]
-password.push(randomSpecial);
-}
-console.log(randomSpecial);
-console.log(password)
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
 
@@ -33,3 +29,7 @@ function generatePassword() {
 
   for (var i = 0; i < passLength; i++)
 }
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
