738e8a5163b7482a5a23c7093a9266c99b0a949c
alaramee <93341641+alaramee@users.noreply.github.com>
2021-12-12 22:18:29 -0500

    add characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,10 @@
 // Assignment code here
 
-
+// Variables for password criteria here
+var lowerCase = ["a", "b", "c", "d", "e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
+var numbers = ["1","2","3","4","5","6","7","8","9","0",]; 
+var symbols = ["!","#","$","%","&","(",")","*","+","-",".","/",":",";","<","=",">","?","@","^","`","|","~"]
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -13,5 +17,19 @@ function writePassword() {
 
 }
 
+if(selectSpecial = 'yes') {
+  var randomSpecial = symbols[Math.floor(Math.random() * symbols.length)]
+password.push(randomSpecial);
+}
+console.log(randomSpecial);
+console.log(password)
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+
+  var passLength = length();
+
+  for (var i = 0; i < passLength; i++)
+}
