47ab011f8b79717bbad44b67f99392d8336e7487
alaramee <93341641+alaramee@users.noreply.github.com>
2021-12-12 23:30:26 -0500

    cleaning up error

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,13 @@
 // Assignment code here
 
 //Undefined variable
-var passLength;
+var initPrompt;
 
 // Variables for password criteria here and criteria mix
 var lowerCase = ["a", "b", "c", "d", "e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
 var numbers = ["1","2","3","4","5","6","7","8","9","0",]; 
 var symbols = ["!","#","$","%","&","(",")","*","+","-",".","/",":",";","<","=",">","?","@","^","`","|","~"]
-var charMix = []
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -17,7 +16,7 @@ generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
-  var passwordGen = [];
+  var passGen = [];
   var passwordText = document.querySelector("#password");
   var passResult = "";
   var initPrompt = window.prompt("Please enter the number of characters would you would like your password to contain. ");
@@ -25,39 +24,39 @@ function writePassword() {
   passwordText.value = newPassword;
 
   //If statements to help navigate through chosen character types
-  if (passLength < 8 | passLength > 128 | isNaN(passLength)) {
+  if (initPrompt < 8 | initPrompt > 128 | isNaN(initPrompt)) {
     window.alert("This answer is not valid. Must between 8 and 128 characters.");
     return initPrompt()
   }
 
   if (confirm("Would you like to include 'Lowercase' letters in your password?")) {
-    charMix = charMix.concat(lowerCase);
+    passGen = passGen.concat(lowerCase);
   }
     
   if (confirm("Would you like to include 'Uppercase' letters in your password?")) {
-    charMix = charMix.concat(upperCase);
+    passGen = passGen.concat(upperCase);
   }
 
   if (confirm("Would you like to includet 'Special Characters' in your password?")) {
-    charMix = charMix.concat(symbols);
+    passGen = passGen.concat(symbols);
   }
 
   if (confirm("Would you like to include 'Numbers' in your password?")) {
-    charMix = charMix.concat(numbers);
+    passGen = passGen.concat(numbers);
   }
 
-  if (charMix.length === 0) {
+  if (passGen.length === 0) {
     alert("Please select at least one character type");
     return; // getPrompts()
   }
 
   else {
     for(var i=0;  i<passCriteria; i++){
-    var random = Math.floor(Math.random()*PasswordGenerator.length);
-    passResult += passwordGen[random];
+    var random = Math.floor(Math.random()*passGen.length);
+    passResult [random];
   }
   
-  return {charLength, charMix}
+  return {charLength, passGen}
    
   }
 }
